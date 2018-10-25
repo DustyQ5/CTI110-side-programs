b9f378dde7bd990d3b2daa314a465e2a0bff26ad
DustyQ5 <43518797+DustyQ5@users.noreply.github.com>
2018-10-25 10:41:53 -0400

    Add files via upload

---
+++ rock paper scissors prototype building.py
@@ -0,0 +1,54 @@
+import random
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+QUIT = 4
+
+def main():
+    rocka = computer_choice()
+    print ('Welcome to Rock, Pape, Scissors!')
+    print ("we're going to play a game!")
+    print ("1) ROCK")
+    print ("2) PAPER")
+    print ("3) SCISSORS")
+    print ("4) QUIT GAME")
+    fwop = player_choice()
+   
+
+#computer game choice
+def computer_choice():
+    rocka = random.randint(1,3)
+    return rocka
+
+
+def player_choice():
+    fwop = int(input('PLEASE ENTER YOUR CHOICE:'))
+    return fwop
+
+
+
+def display_menu
+    print ('Welcome to Rock, Pape, Scissors!')
+    print ("we're going to play a game!")
+    print ("1) ROCK")
+    print ("2) PAPER")
+    print ("3) SCISSORS")
+    print ("4) QUIT GAME")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
+    
